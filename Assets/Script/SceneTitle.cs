5090f9bfd4c0ef4d3ec677b2827837bdef0028c3
cielnelios <61488902+cielnelios@users.noreply.github.com>
2023-06-04 17:36:11 +0900

    UI 닫기 구현에서 UI 관리 스택을 이용하도록 변경

--- Assets/Script/SceneTitle.cs
+++ Assets/Script/SceneTitle.cs
@@ -17,11 +17,27 @@ public class SceneTitle : MonoBehaviour
         _sceneTitleText.text = thisScene.name;
 
         // X 버튼에 씬 닫기 등록
-        _closeSceneButton.onClick.AddListener(() => this.DelUIScene(thisScene));
+        //_closeSceneButton.onClick.AddListener(() => this.DelUIScene(thisScene));
+        _closeSceneButton.onClick.AddListener(() => this.DelUIScene());
     }
 
     private void DelUIScene(Scene scene)
     {
+        // 직접 씬을 지우는 구현
         SceneManager.UnloadSceneAsync(scene);
+    }
+
+    private void DelUIScene()
+    {
+        //UI Master를 찾아가서 거기의 씬 삭제 함수를 호출하는 구현.
+        //UI Master가 열린 UI의 스택을 가지고 있다.
+        GameObject[] nextSceneRootObject = SceneManager.GetSceneByBuildIndex(0).GetRootGameObjects();
+        foreach (GameObject RootGameObject in nextSceneRootObject)
+        {
+            if (RootGameObject.CompareTag("UIMaster"))
+            {
+                RootGameObject.GetComponent<UIMasterIndex>().DelUIScene();
+            }
+        }
     }
 }
